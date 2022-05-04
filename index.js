f37e161fa2e11eabc21402074bbb0b14e2a3b8fc
ning zhou <bim2016@gmail.com>
2022-05-05 00:32:26 +0800

    3rd commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function test(string) {
 
 function dueDate(req, filter) {
   let d1 = new Date(req.query.due_date);
-  let d2 = new Date(d1.getTime() + 86399999);
+  let d2 = new Date(d1.getTime() + 8639999);
   filter.due_date = [d1, d2];
 }
 
